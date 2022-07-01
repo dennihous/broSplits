6e207f0ec325c29d94f3704e886cb43b5236dae8
dennis <denhouston1@icloud.com>
2022-07-01 13:40:59 +0100

    added routes

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static(__dirname + "/static"));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  console.log(process.cwd())
+  res.sendFile('/Users/dennishouston/Projects/broSplits/broSplits/index.html')
 })
+
+app.get('/')
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
