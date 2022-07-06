2fc87c365dce88c1b74acf2ef6e3807c3cf67f91
dennis <denhouston1@icloud.com>
2022-07-06 23:27:19 +0100

    app.js edited/shortened

--- app.js
+++ app.js
@@ -5,31 +5,28 @@ const port = 3000
 app.use(express.static(__dirname + "/static"));
 
 app.get('/', (req, res) => {
-  console.log(process.cwd())
   res.sendFile(process.cwd().concat('/routes/index.html'))
 })
 
 app.get('/style.css', (req, res) => {
-  res.sendFile('/Users/dennishouston/Projects/broSplits/broSplits/style.css')
+  res.sendFile(process.cwd().concat('/style.css'))
 })
 
 app.get('/about', (req, res) => {
-  res.sendFile('/Users/dennishouston/Projects/broSplits/broSplits/routes/about.html')
+  res.sendFile(process.cwd().concat('/routes/about.html'))
 })
 
 app.get('/services', (req, res) => {
-  res.sendFile('/Users/dennishouston/Projects/broSplits/broSplits/routes/services.html')
+  res.sendFile(process.cwd().concat('/routes/services.html'))
 })
 
 app.get('/BMI', (req, res) => {
-  res.sendFile('/Users/dennishouston/Projects/broSplits/broSplits/routes/bmi.html')
+  res.sendFile(process.cwd().concat('/routes/bmi.html'))
 })
 
 app.get('/contact', (req, res) => {
-  res.sendFile('/Users/dennishouston/Projects/broSplits/broSplits/routes/contact.html')
+  res.sendFile(process.cwd().concat('/routes/contact.html'))
 })
-
-app.get('/')
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
