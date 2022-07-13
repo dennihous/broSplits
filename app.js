a44ce6ddc9230473339068781320a2f0e21dd19a
dennis <denhouston1@icloud.com>
2022-07-13 16:53:46 +0100

    removed static pages and implemented handlebars basic

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express')
+const engine = require('express-handlebars').engine
 const app = express()
 const port = process.env.PORT || 3000
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 app.use(express.static(__dirname + "/static"));
 
 app.get('/', (req, res) => {
-  res.sendFile(process.cwd().concat('/routes/index.html'))               
+  // res.sendFile(process.cwd().concat('/routes/index.html')) 
+  res.render('home', {name: 'Dennis'})              
 })
 
 app.get('/style.css', (req, res) => {
